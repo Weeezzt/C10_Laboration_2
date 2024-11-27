288136cc47c55d61be6ff70f54be203cb0e42ca8
Weeezzt <william.vesterberg@lerniastudent.se>
2024-11-27 12:10:48 +0100

    Initial commit

---
+++ src/main/java/org/example/c10_laboration_2/C10Laboration2Application.java
@@ -0,0 +1,13 @@
+package org.example.c10_laboration_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C10Laboration2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C10Laboration2Application.class, args);
+    }
+
+}
